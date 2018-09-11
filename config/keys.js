bd3a957dbb5f52955f4886dc78b107094caea5ae
Milad Abbasi <miladdarren75@gmail.com>
2018-09-11 23:50:13 +0430

    Edit environment variables

--- config/keys.js
+++ config/keys.js
@@ -1,27 +1,43 @@
-const env = require('../env');
+const secretOrKey = process.env.SECRET_OR_KEY;
+const jwtExpires = process.env.JWT_EXPIRES;
+
+const googleClientID = process.env.GOOGLE_AUTH_ID;
+const googleClientSecret = process.env.GOOGLE_AUTH_SECRET;
+const googleCallbackURL =
+    process.env.GOOGLE_AUTH_URL || 'http://localhost:3000/auth/google';
+
+const githubClientID = process.env.GITHUB_AUTH_ID;
+const githubClientSecret = process.env.GITHUB_AUTH_SECRET;
+const githubCallbackURL =
+    process.env.GITHUB_AUTH_URL || 'http://localhost:3000/auth/github';
+
+const linkedinClientID = process.env.LINKEDIN_AUTH_ID;
+const linkedinClientSecret = process.env.LINKEDIN_AUTH_SECRET;
+const linkedinCallbackURL =
+    process.env.LINKEDIN_AUTH_URL || 'http://localhost:3000/auth/linkedin';
 
 module.exports = {
-    secretOrKey: env.secretOrKey,
-    saltFactor: env.saltFactor,
-    jwtExpires: { expiresIn: env.jwtExpires },
+    secretOrKey: secretOrKey,
+    saltFactor: 10,
+    jwtExpires: { expiresIn: jwtExpires },
     cookieOptions: {
         expires: new Date(Date.now() + 3600000)
     },
     googleAuth: {
-        clientID: env.googleClientID,
-        clientSecret: env.googleClientSecret,
-        callbackURL: env.googleCallbackURL
+        clientID: googleClientID,
+        clientSecret: googleClientSecret,
+        callbackURL: googleCallbackURL
     },
     githubAuth: {
-        clientID: env.githubClientID,
-        clientSecret: env.githubClientSecret,
-        callbackURL: env.githubCallbackURL,
+        clientID: githubClientID,
+        clientSecret: githubClientSecret,
+        callbackURL: githubCallbackURL,
         scope: ['user:email'] // Fetch private emails
     },
     linkedinAuth: {
-        clientID: env.linkedinClientID,
-        clientSecret: env.linkedinClientSecret,
-        callbackURL: env.linkedinCallbackURL,
+        clientID: linkedinClientID,
+        clientSecret: linkedinClientSecret,
+        callbackURL: linkedinCallbackURL,
         scope: ['r_emailaddress', 'r_basicprofile']
     }
 };
