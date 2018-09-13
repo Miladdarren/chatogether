022944ad6ae580efd55267d385171fe91727457b
Milad Abbasi <miladdarren75@gmail.com>
2018-09-13 22:44:31 +0430

    Update home component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,52 +1,12 @@
 ﻿import { Component, OnInit } from '@angular/core';
-import { first } from 'rxjs/operators';
-import { CookieService } from 'ngx-cookie';
-import * as jwtDecode from 'jwt-decode';
+import { AuthenticationService } from '../_services';
 
-import { User } from '../_models';
-import { UserService } from '../_services';
-
-@Component({ templateUrl: 'home.component.html' })
+@Component({
+    templateUrl: 'home.component.html',
+    styleUrls: ['./home.component.scss']
+})
 export class HomeComponent implements OnInit {
-    currentUser: User;
-    users: User[] = [];
+    constructor(public authenticationService: AuthenticationService) {}
 
-    constructor(
-        private userService: UserService,
-        private cookieService: CookieService
-    ) {
-        // Getting jwt from cookie
-        const accessToken = this.cookieService.get('accessToken');
-        // Decoding jwt token
-        const decodedUser = jwtDecode(accessToken);
-        decodedUser.token = accessToken;
-        this.currentUser = decodedUser;
-    }
-
-    ngOnInit() {
-        this.loadAllUsers();
-    }
-
-    deleteUser(id: number) {
-        this.userService
-            .delete(id)
-            .pipe(first())
-            .subscribe(() => {
-                this.loadAllUsers();
-            });
-    }
-
-    private loadAllUsers() {
-        this.userService
-            .getAll()
-            .pipe(first())
-            .subscribe(users => {
-                this.users = users;
-            });
-    }
-
-    logout() {
-        // remove user from cookie to log user out
-        this.cookieService.remove('accessToken');
-    }
+    ngOnInit() {}
 }
